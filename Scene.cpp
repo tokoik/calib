c67fef6041157108af9a48a6379a52e243f805b1
Kohe Tokoi <tokoi@wakayama-u.ac.jp>
2024-05-10 16:28:24 +0900

    add Scene class

---
+++ Scene.cpp
@@ -0,0 +1,37 @@
+﻿///
+/// シーンの描画クラスの実装
+///
+/// @file
+/// @author Kohe Tokoi
+/// @date November 15, 2022
+///
+#include "Scene.h"
+
+//
+// コンストラクタ
+//
+Scene::Scene() :
+  light{ *menu.light.get() },
+  model{ *menu.model.get() },
+  shader{ *menu.shader.get() }
+{
+}
+
+//
+// デストラクタ
+//
+Scene::~Scene()
+{
+}
+
+//
+// 描画する
+//
+void Scene::draw(const GgMatrix& mp, const GgMatrix& mv) const
+{
+  // シェーダプログラムを指定する
+  shader.use(mp, mv, light);
+
+  // 図形を描画する
+  model.draw();
+}
---
+++ Scene.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+
+///
+/// シーンの描画クラスの定義
+///
+/// @file
+/// @author Kohe Tokoi
+/// @date November 15, 2022
+///
+
+// 補助プログラム
+#include "gg.h"
+using namespace gg;
+
+///
+/// シーンの描画クラス
+///
+class Scene
+{
+  // シェーダ
+  const GgSimpleShader& shader;
+
+  // 光源
+  const GgSimpleShader::LightBuffer& light;
+
+  // モデル
+  const GgSimpleObj& model;
+
+public:
+
+  ///
+  /// コンストラクタ
+  ///
+  Scene();
+
+  ///
+  /// デストラクタ
+  ///
+  virtual ~Scene();
+
+  ///
+  /// シーンを描画する
+  ///
+  void draw(const GgMatrix& mp, const GgMatrix& mv) const;
+};
